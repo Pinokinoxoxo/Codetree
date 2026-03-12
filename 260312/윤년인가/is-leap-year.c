f51fbb0f37a234f80f6dde24ea88353b7ac81a64
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-03-12 14:20:20 +0900

    Title: 윤년인가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260312/윤년인가/is-leap-year.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int year;
+    scanf("%d", &year);
+    if (year % 4 == 0) {
+        if ((year % 100 == 0) && (year % 400 != 0)) {
+            printf("false");
+        } else printf("true");
+    }
+    else printf("false");
+    return 0;
+}
