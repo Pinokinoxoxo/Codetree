775b30db10d272711341366314881010f46e6e20
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-03-13 23:17:46 +0900

    Title: 100점 도달하기, Time: 59ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260313/100점 도달하기/reach-100-point.py
@@ -0,0 +1,21 @@
+n = int(input())
+a = n
+for i in range(101 - a):
+    if n >= 90:
+        print('A', end=" ")
+        n = n + 1
+    elif n >= 80:
+        print('B', end=" ")
+        n = n + 1
+    elif n >= 70:
+        print('C', end=" ")
+        n = n + 1
+    elif n <= 60:
+        print('D', end=" ")
+        n = n + 1
+    else:
+        print('F', end=" ")
+        n = n + 1
+    
+
+
