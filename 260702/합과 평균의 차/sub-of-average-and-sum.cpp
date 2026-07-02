526101fde04a6352f0490454cfcf5e08ca46c7fd
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-02 22:25:47 +0900

    [Passed] 합과 평균의 차 (2ms, 0MB)

---
+++ 260702/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c;
+    cin >> a >> b >> c;
+    int d = a + b + c;
+    int e = (a + b + c) / 3;
+    cout << d;
+    cout << "\n";
+    cout << e;
+    cout << "\n";
+    cout << d - e;
+    return 0;
+}
