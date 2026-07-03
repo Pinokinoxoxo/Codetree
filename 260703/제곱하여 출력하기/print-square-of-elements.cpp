465aef03f458696554b9ccf9adb43a81baee1dff
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-03 14:36:20 +0900

    [Passed] 제곱하여 출력하기 (2ms, 0MB)

---
+++ 260703/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int j = 0; j < n; j++) {
+        cout << a[j] * a[j] << " ";
+    }
+    return 0;
+}
