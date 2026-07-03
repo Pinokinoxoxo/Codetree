89757afecfefdb8b831683c0b4406e908ce9b222
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-03 14:40:16 +0900

    [Passed] 일의 자리 배열 (2ms, 0MB)

---
+++ 260703/일의 자리 배열/array-with-units.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    int arr[10];
+
+    cin >> a >> b;
+    arr[0] = a;
+    arr[1] = b;
+    cout << arr[0] << " " << arr[1] << " ";
+    for (int i = 2; i < 10; i++) {
+        arr[i] = (arr[i-2] + arr[i-1]) % 10;
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
