5efa95c0ada215fa633605967397186ead8c4e9b
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-04 13:35:01 +0900

    [Passed] 직각 삼각형 출력 (2ms, 0MB)

---
+++ 260704/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    for (int i = 1; i < n + 1; i++) {
+        for (int j = 0; j < i * 2 - 1; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
