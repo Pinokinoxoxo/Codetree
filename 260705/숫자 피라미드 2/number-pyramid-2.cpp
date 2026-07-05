ab259f599a7c73488d817d9d02b4e18813f34371
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-05 16:08:16 +0900

    [Passed] 숫자 피라미드 2 (2ms, 0MB)

---
+++ 260705/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    int cnt = 1;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << cnt << " ";
+            cnt++;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
