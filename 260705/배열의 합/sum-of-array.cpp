8e8cb980fa2e4f502f1b0e14aeb97bf8c7f595b0
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-05 17:06:07 +0900

    [Passed] 배열의 합 (3ms, 0MB)

---
+++ 260705/배열의 합/sum-of-array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int mat[4][4];
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> mat[i][j];
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        int cnt = 0;
+        for (int j = 0; j < 4; j++) {
+            cnt += mat[i][j];
+        }
+        cout << cnt << "\n";
+    }
+    return 0;
+}
