c90a50e51eec9b07a45983b946b0341b9bb9bec9
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-06 23:14:01 +0900

    [Passed] 길이 변형 후 사각형 넓이 구하기 (2ms, 0MB)

---
+++ 260706/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, m;
+    cin >> n >> m;
+    n = n + 8;
+    m = 3 * m;
+    cout << n << endl << m << endl << n * m;
+    return 0;
+}
