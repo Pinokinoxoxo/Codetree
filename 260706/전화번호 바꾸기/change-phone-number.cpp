7072ef7d7bd749fb595e100aa4426b6b3a4c3e82
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-06 22:30:12 +0900

    [Passed] 전화번호 바꾸기 (2ms, 0MB)

---
+++ 260706/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int tmp, pf, pl;
+    cin >> tmp;
+    cin.get();
+    cin >> pf;
+    cin.get();
+    cin >>pl;
+    cout << "010-" << pl << "-" << pf;
+    return 0;
+}
