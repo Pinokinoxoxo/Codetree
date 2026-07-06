6f2e28b5f535570319cc11e2041dd012106e0c9e
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-06 22:26:15 +0900

    [Passed] 입력받아 출력 2 (2ms, 0MB)

---
+++ 260706/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int idf, idl;
+    cin >> idf;
+    cin.get();
+    cin >> idl;
+    cout << idf << idl;
+    return 0;
+}
