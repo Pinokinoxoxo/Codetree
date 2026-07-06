44e749fc49694f9ae6372ddc4fb9bb3b1d023392
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-06 22:00:23 +0900

    [Passed] 길이 단위 환산하기 (2ms, 0MB)

---
+++ 260706/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double ft;
+    cin >> ft;
+    cout << fixed;
+    cout.precision(1);
+    cout << ft * 30.48;
+    return 0;
+}
