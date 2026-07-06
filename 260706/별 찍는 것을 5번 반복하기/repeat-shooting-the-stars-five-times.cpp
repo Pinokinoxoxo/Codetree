a2478e7ff03f7fab52c9325a33a5d0e0e3e10f61
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-06 23:32:54 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (2ms, 0MB)

---
+++ 260706/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void print10star() {
+    for (int i = 0; i < 10; i++) {
+        cout << "*";
+    }
+    cout << endl;
+}
+int main() {
+    // Please write your code here.
+    for (int i = 0; i < 5; i++) {
+        print10star();
+    }
+    return 0;
+}
