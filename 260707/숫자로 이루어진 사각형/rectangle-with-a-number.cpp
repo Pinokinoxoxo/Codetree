04b90e196393439daddeeb4754f0c9555f5d8dda
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-07 11:30:15 +0900

    [Passed] 숫자로 이루어진 사각형 (2ms, 0MB)

---
+++ 260707/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void printbox(int n) {
+    int cnt = 1;
+    for (int i =0; i< n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << cnt << " ";
+            cnt++;
+            if (cnt > 9) {
+                cnt = 1;
+            }
+        }
+        cout << endl;
+    }
+}
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    printbox(n);
+    return 0;
+}
