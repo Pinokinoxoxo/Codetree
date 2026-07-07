b560ab1f5943448ad127bf5b8acca6dd5cde3800
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-07 11:22:50 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ 260707/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void printit() {
+    cout << "12345^&*()_" << endl;
+}
+
+int main() {
+    cin >> N;
+
+    // Please write your code here.
+    for (int i = 0; i < N; i++) {
+        printit();
+    }
+
+    return 0;
+}
