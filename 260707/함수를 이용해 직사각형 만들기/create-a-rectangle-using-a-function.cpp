b1bb912c228ae9104f6646ad675b5a9fd013fae5
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2026-07-07 11:24:57 +0900

    [Passed] 함수를 이용해 직사각형 만들기 (2ms, 0MB)

---
+++ 260707/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void printbox(int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << 1;
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    // Please write your code here.
+    int n, m;
+    cin >> n >> m;
+    printbox(n, m);
+    return 0;
+}
